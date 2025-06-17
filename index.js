7b2a5df7e1b6c46c4c07854ed224cc814464f9fd
Rohini0518 <rohinipolina0518@gmail.com>
2025-06-17 22:45:26 +0530

    updated port config

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-// app.listen(port, "localhost", () => {
-//   console.log(`running in ${port} port`);
-// });
 app.listen(port, "0.0.0.0", () => {
   console.log(`running on port ${port} (0.0.0.0)`);
 });
@@ -20,5 +17,5 @@ app.get("/", (req, res) => res.send("Backendserver started"));
 app.use("/api", taskRoutes);
 
 mongoose.connect(process.env.MONGODB_URL)
-.then(() => console.log("✅ Connected to MongoDB Atlas"))
-.catch(err => console.error("❌ Connection error:", err));
+.then(() => console.log(" Connected to MongoDB Atlas"))
+.catch(err => console.error(" Connection error:", err));
